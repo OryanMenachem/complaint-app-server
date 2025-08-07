be0705c6068be484bf9fdb478f3a7bd8e8041206
Oryan Menachem <om3160204@gmail.com>
2025-08-07 12:06:51 +0300

    feat: generate Complaint module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ComplaintModule } from './complaint/complaint.module';
 
 @Module({
-  imports: [],
+  imports: [ComplaintModule],
   controllers: [AppController],
   providers: [AppService],
 })
