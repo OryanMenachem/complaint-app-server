f3a2f9bca5efceec78d7d6c2f04caf6c59101111
Oryan Menachem <om3160204@gmail.com>
2025-08-07 12:15:37 +0300

    chore(server): add env variables and MongoDB connection setup
    - Added PORT and MONGODB_URI variables to .env file
    - Implemented MongoDB connection in NestJS server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ComplaintModule } from './complaint/complaint.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ComplaintModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ComplaintModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI!),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
